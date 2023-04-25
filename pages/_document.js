c534c28ad2642a6c3d8b50b6d78b0f9dce2bfefd
JRH89 <thegemlins@gmail.com>
2023-04-25 01:52:38 -0700

    ok

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import MyImageGallery from '@/components/ImageGallery'
 import { Html, Head, Main, NextScript } from 'next/document'
 import React from 'react'
+
 export default function Document() {
   return (
     <Html lang="en">
+      <meta charset="UTF-8"></meta>
+      <link rel="shortcut icon" href="images/Hooker Hill (1).jpg"></link>
+      <title>AdventuresInFallHollow</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <Head />
       <body>
         <Main />
-        
         <NextScript />
       </body>
     </Html>
--- pages/gallery.js
+++ pages/gallery.js
@@ -2,9 +2,9 @@ import MyImageGallery from '@/components/ImageGallery'
 
 const GalleryPage = () => {
   return (
-      <div>
-          <h1 className='pt-5 text-center sm:text-2xl md:text-3xl'>Fall Hollow Image Gallery</h1>
-      <MyImageGallery />
+    <div>
+        <h1 className='pt-5 text-center sm:text-2xl md:text-3xl'>Fall Hollow Image Gallery</h1>
+        <MyImageGallery />
     </div>
   );
 };
