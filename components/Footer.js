b3fd73728b7e4079cfcf1e8457308077044322c1
JRH89 <thegemlins@gmail.com>
2023-04-25 15:06:12 -0700

    ok

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,21 @@
+import Link from "next/link"
 import React from "react"
 
 export default function Footer() {
   return (
     <div className="flex justify-center items-center gap-3 py-3">
-          <a href="https://twitter.com/gamedevjared89" target="_blank">
-              <i className="fa-brands fa-twitter duration-300 hover:opacity-30 cursor-pointer text-violet-900 text-3xl"></i>
-          </a>
-          <a href="https://linkedin.com/in/jaredhooker" target="_blank">
-              <i className="fa-brands fa-linkedin duration-300 hover:opacity-30 cursor-pointer text-violet-900 text-3xl"></i>
-          </a>
-          <a href="https://github.com/jrh89" target="_blank">
-              <i className="fa-brands fa-github duration-300 hover:opacity-30 cursor-pointer text-violet-900 text-3xl"></i>
-          </a>
-          <a href="https://jaredsportfolio.netlify.app" target="_blank">
-              <i className="fa-solid fa-house-chimney duration-300 hover:opacity-30 cursor-pointer text-violet-900 text-3xl"></i>
-          </a>
+          <Link href="https://twitter.com/gamedevjared89" target="_blank">
+              <i className="fa-brands fa-twitter duration-300 hover:opacity-30 cursor-pointer text-violet-600 text-3xl"></i>
+          </Link>
+          <Link href="https://linkedin.com/in/jaredhooker" target="_blank">
+              <i className="fa-brands fa-linkedin duration-300 hover:opacity-30 cursor-pointer text-violet-600 text-3xl"></i>
+          </Link>
+          <Link href="https://github.com/jrh89" target="_blank">
+              <i className="fa-brands fa-github duration-300 hover:opacity-30 cursor-pointer text-violet-600 text-3xl"></i>
+          </Link>
+          <Link href="https://jaredsportfolio.netlify.app" target="_blank">
+              <i className="fa-solid fa-house-chimney duration-300 hover:opacity-30 cursor-pointer text-violet-600 text-3xl"></i>
+          </Link>
     </div>
   )
 }
