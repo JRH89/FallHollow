de6a39d33a392781d12c42544c736815ecfba64b
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-05-20 01:57:13 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ export default function Document() {
       <meta property='og:image' content='https://github.com/JRH89/FallHollow/blob/main/public/images/fh/IG_1.png' />
       <meta property='og:description' content='Unreal Engine 5 Project'/>
       <meta property='og:title' content='Fall Hollow' /> 
+<meta property="description" content="Unreal Engine 5 Project" />
       <meta property='og:type' content='website' />
       <meta property='og:url' content='https://fall-hollow.vercel.app/' />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
