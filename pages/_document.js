cda267572b238790c4eadc3a5a02cb6decae690c
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-05-20 01:53:32 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ import Footer from '@/components/Footer'
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
       <meta charSet="UTF-8"></meta>
       <link rel="shortcut icon" href="images/Hooker Hill (1).jpg"></link>
       <title>AdventuresInFallHollow</title>
@@ -15,7 +16,7 @@ export default function Document() {
       <meta property='og:url' content='https://fall-hollow.vercel.app/' />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-      <Head />
+      </Head>
       <body>
         <Main />
         <NextScript />
